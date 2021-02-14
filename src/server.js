aacad48aac4279125ca98ac2d1ebd54d7ac69f30
minhthanh95ptit <minhthanh95ptit@gmail.com>
2021-02-14 20:38:34 +0700

    [1.0.0] Connect DB

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 // var express = require("express")
 import express from "express"
- 
+import connectDB from "./config/connectDB"
+
 let app = express()
+
+//CONNECT TO MONGODB
+connectDB()
 
 let hostname = "localhost"
 let port = 8017
@@ -10,6 +14,6 @@ app.get("/",(req,res) => {
     res.send("<h1>Hello Pham Minh Thanh</h1>")
 });
 
-app.listen(port, hostname,() => {
-    console.log(`Running at ${hostname}:${port}`)
+app.listen(process.env.APP_PORT, process.env.APP_HOST,() => {
+    console.log(`Running at ${process.env.APP_HOST}:${process.env.APP_PORT}`)
 })
