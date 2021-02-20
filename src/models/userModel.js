71db7db1cbc9703c66a19745b2e1a9000ec57c7f
minhthanh95ptit <minhthanh95ptit@gmail.com>
2021-02-20 21:54:26 +0700

    [1.0.0] Update avatar Backend

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -59,6 +59,9 @@ UserSchema.statics = {
   },
   findByGoogleUid(uid){
     return this.findOne({"google.uid": uid}).exec()
+  },
+  updateUser(id, item){
+    return this.findByIdAndUpdate(id, item).exec()
   }
 }
 
